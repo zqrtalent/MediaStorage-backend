630ab34ff11dea9c1a0ca4f5a369bd23d7f4313f
zqrtalent <zaqroo@gmail.com>
2018-06-10 21:54:48 +0200

    WebApp -> bind to any address

--- MediaStreamingApp/Program.cs
+++ MediaStreamingApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -37,7 +38,9 @@ namespace MediaStreamingApp
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
+                .UseKestrel( options => {
+                    options.Listen(IPAddress.Any, 5001);
+                })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
